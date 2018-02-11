8e52631aa1da9478fd268498d8fa6a5a5b6dafaa
Lakshmi Bhallamudi <lakshmigayatri.bhallamudi@gmail.com>
2018-02-11 15:43:16 +0530

    Java - ConstructorValidModifiers -  Public, private ,protected and default modifiers are only valid modifiers for Constructor

---
+++ src/com/m/g/java/basics/PossibleModifiersOfAConstructor.java
@@ -0,0 +1,31 @@
+package com.m.g.java.basics;
+
+/**
+ * The allowed Modifiers of a constructor are public,protected,private,Default
+ * final,static,abstract and remaining modifiers except above four are not
+ * allowed modifiers for constructor.If we try to use final/static/abstract it
+ * will throw compile time error.
+ * 
+ * Usually class modifier will be constructor modifier. If we  specify the modifiers for constructor it will be considered
+ * 
+ * @author lbhallamudi
+ *
+ */
+
+public class PossibleModifiersOfAConstructor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PossibleModifiersOfAConstructor pf = new PossibleModifiersOfAConstructor(6);
+
+	}
+
+	public PossibleModifiersOfAConstructor() {
+		System.out.println("no arg Constructor");
+	}
+
+	public PossibleModifiersOfAConstructor(int i) {
+		System.out.println("arg constructor");
+	}
+
+}
