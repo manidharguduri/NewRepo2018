a0aeccff54056f1a323cf7e0a980d78c716a983d
Manidhar.Guduri <Manidhar.Guduri@target.com>
2018-02-13 08:51:43 +0530

    Main : Guidelines for maintaining and committing the code in the repository

--- src/com/m/g/main/Simple.java
+++ src/com/m/g/main/Simple.java
@@ -4,6 +4,21 @@ public class Simple {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to ML");
+        System.out.println("Guide Lines for maintaining and committing the code in this repository...");
+        System.out.println("***********************************************************************************");
+        System.out.println("***********************************************************************************");
+        System.out.println("File name or the class name should contain only alphabets [a-z][A-Z] and no special characters.");
+        System.out.println("===================================================================================");
+        System.out.println("The inner classes class name that are defined, should be prefixed with the outer class class name");
+        System.out.println("===================================================================================");
+        System.out.println("when committing, commit individual files with necessary description");
+        System.out.println("Commit message forrmat    \" Concept Type : Sub Concept : Description about the concept \"");
+        System.out.println("Commit message example    \" Java : Inheritance : method overriding \"");
+        System.out.println("Commit message forrmat    \" LinkedList : Circular linked list : Find the loop in the linked list \"");
+        System.out.println("===================================================================================");
+        System.out.println("Commit one file at a time ");
+        System.out.println("===================================================================================");
+        System.out.println("");
+
     }
 }
