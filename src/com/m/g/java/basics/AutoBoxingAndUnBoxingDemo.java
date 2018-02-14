bcb01be23f19dd89b5ea6cf1451c6e25d5a6f7d9
Lakshmi Bhallamudi <lakshmigayatri.bhallamudi@gmail.com>
2018-02-14 16:47:09 +0530

    java - AutoBoxing/unBoxing - Different Methods

---
+++ src/com/m/g/java/basics/AutoBoxingAndUnBoxingDemo.java
@@ -0,0 +1,13 @@
+package com.m.g.java.basics;
+
+public class AutoBoxingAndUnBoxingDemo {
+
+	public static void main(String[] args) {
+		
+		//Methods of wrapper Classes
+		// 1.parseInt(Wrapper Object) ==> Converts Wrapper Object to Primitive
+		// 2.valueOf("String") or valueOf(primitive) ==> Converts String OR primitive to Wrapper Object 
+		// 3.intValue() ==> Converts Wrapper Object to int value.
+		// 4.toString() ==> Converts primitive or wrapper Objetc to String
+		}
+}
