84b9529d92ffcdb90e7107f788f433d5c519ce8a
Lakshmi Bhallamudi <lakshmigayatri.bhallamudi@gmail.com>
2018-02-18 16:34:36 +0530

    Java : Exception : Custom Run time Exception Implementation.

--- src/com/m/g/java/exception/ThrowingRuntimeException.java
+++ src/com/m/g/java/exception/ThrowingRuntimeException.java
@@ -2,26 +2,25 @@ package com.m.g.java.exception;
 
 public class ThrowingRuntimeException {
 
+	public static void main(String[] args) {
+		ThrowingRuntimeException t = new ThrowingRuntimeException();
+		// t.simpleMethod("This is thrown from custom run time exception");
+		t.simpleMethod(null);
+	}
 
-    public static void main(String[] args) {
+	public void simpleMethod(String s) {
+		if (s == null || s == "")
+			throw new StringNotFoundRuntimeException(s);
+		else {
+			System.out.println(s);
+		}
+	}
 
+}
 
-    }
+class StringNotFoundRuntimeException extends RuntimeException {
 
-    // runtime exception no need to specify the throws to a method.
-
-    public void simpleException() throws Exception {
-        try
-        {
-
-        }
-        catch(Exception ex)
-        {
-            throw new Exception();
-        }
-        finally
-        {
-
-        }
-    }
+	StringNotFoundRuntimeException(String s) {
+		// System.out.println(s);
+	}
 }
