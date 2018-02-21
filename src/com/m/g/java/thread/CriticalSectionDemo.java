d38ae88d9d2399641333d7eda46146a4848797f5
Lakshmi Bhallamudi <lakshmigayatri.bhallamudi@gmail.com>
2018-02-21 16:00:11 +0530

    Java : Threads : Critical Section using Global variable in SharedResource

---
+++ src/com/m/g/java/thread/CriticalSectionDemo.java
@@ -0,0 +1,47 @@
+package com.m.g.java.thread;
+
+public class CriticalSectionDemo {
+
+	static SharedResource sr = new SharedResource();
+
+	static Thread t1 = new Thread() {
+		public void run() {
+			sr.getSum(10);
+		}
+
+	};
+
+	static Thread t2 = new Thread() {
+		public void run() {
+			sr.getSum(10);
+		}
+
+	};
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		t1.start();
+		t2.start();
+		/*
+		 * try { t1.join(); } catch (InterruptedException e) { // TODO
+		 * Auto-generated catch block e.printStackTrace(); }
+		 */
+
+	}
+
+}
+
+class SharedResource {
+	int sum = 0;
+
+	public void getSum(int n) {
+
+		for (int index = 0; index < n; index++) {
+
+			sum = sum + index;
+		}
+		System.out.println(Thread.currentThread().getName() + "sum:: " + sum);
+	}
+
+}
