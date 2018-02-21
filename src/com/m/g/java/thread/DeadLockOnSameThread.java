52b5b56bc5577145c0ed0d744f19cdfabd7ed361
Lakshmi Bhallamudi <lakshmigayatri.bhallamudi@gmail.com>
2018-02-21 19:49:18 +0530

    Java : Thread : DeadLock on same Thread

---
+++ src/com/m/g/java/thread/DeadLockOnSameThread.java
@@ -0,0 +1,17 @@
+package com.m.g.java.thread;
+
+public class DeadLockOnSameThread {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Main thread is executing..");
+		try {
+			Thread.currentThread().join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Never executes this since dead lock occured");
+	}
+
+}
