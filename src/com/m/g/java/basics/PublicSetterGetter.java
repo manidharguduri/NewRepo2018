a8dd2c9cec59278bc639f99d78c4c132f1a2f53c
Manidhar.Guduri <Manidhar.Guduri@target.com>
2018-02-25 13:38:42 +0530

    Java : Basic : Public Variable can have setter and getter but which is of not Sense

---
+++ src/com/m/g/java/basics/PublicSetterGetter.java
@@ -0,0 +1,37 @@
+package com.m.g.java.basics;
+
+/**
+ * Public varaibles can have setters and getters.
+ * But the oops principles data hiding will be violated
+ * if the varaibles are public since it can be accessed b y outside world.
+ *
+ */
+
+public class PublicSetterGetter {
+
+    public String name;
+    public String password;
+    public  static  void main(String[] args)
+    {
+
+        PublicSetterGetter ps=new PublicSetterGetter();
+        ps.setName("gayi");
+        ps.setName("mani");
+
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
