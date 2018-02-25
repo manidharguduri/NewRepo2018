da0f0e880ae2a2677da4f5cfe00a17b1c11ed74c
Manidhar.Guduri <Manidhar.Guduri@target.com>
2018-02-25 14:40:21 +0530

    Java : Thread : Exchanger Example in Threads

---
+++ src/com/m/g/threads/advanced/ExchangerDemo.java
@@ -0,0 +1,48 @@
+package com.m.g.threads.advanced;
+/**
+ * Exchanger allows exchanging of objects between two threads.
+ * First thread reaches the exchange points and waits till the second thread
+ * reaches the exchange point and then exchange the values.
+ */
+
+import java.util.concurrent.Exchanger;
+
+public class ExchangerDemo {
+
+    public static void main(String[] args) {
+        Exchanger<String> exchanger = new Exchanger<String>();
+        Thread t1 = new Thread(new ExchangerTask(exchanger, " Hi Mani "));
+        Thread t2 = new Thread(new ExchangerTask(exchanger, " Hello Gayi"));
+        t1.setName("First Thread");
+        t2.setName("Second Thread");
+        t1.start();
+        t2.start();
+    }
+}
+
+class ExchangerTask implements Runnable {
+    Exchanger<String> exchanger;
+    String name;
+
+    ExchangerTask(Exchanger exchanger, String name) {
+        this.exchanger = exchanger;
+        this.name = name;
+    }
+
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + " Name before Exchange " + name);
+        try {
+            name = exchanger.exchange(name);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread().getName() + " Name after Exchange  " + name);
+    }
+
+}
+
+
+
+
+
+
