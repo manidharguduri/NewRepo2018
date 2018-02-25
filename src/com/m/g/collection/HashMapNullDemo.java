d0d0ea3759602072b24ac314e2dc4346048c5890
Manidhar.Guduri <Manidhar.Guduri@target.com>
2018-02-25 13:29:07 +0530

    Java : Collection : HashMap null Key Index is Zero.

---
+++ src/com/m/g/collection/HashMapNullDemo.java
@@ -0,0 +1,27 @@
+package com.m.g.collection;
+/***
+ * Whenever a key is null. HashMap places
+ * the key in '0'index.HashMap allows only one
+ * key and whenever another null key wants to be placed
+ * It replaces the value of earlier null.
+ * This is as per the HashMap properties.
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapNullDemo {
+
+    public static void main(String[] args) {
+
+        Map<Integer, String> map = new HashMap<Integer, String>();
+        map.put(1, "gayi");
+        map.put(2, "mani");
+        map.put(null, "family");
+        map.put(null,"Love");
+        System.out.println(map);
+
+    }
+
+
+}
