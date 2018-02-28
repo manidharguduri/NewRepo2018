9f4119bbeb584974fa3b20344347351439416b9b
Manidhar.Guduri <Manidhar.Guduri@target.com>
2018-03-01 00:42:25 +0530

    Java : Basic : Method overriding throws clause

---
+++ src/com/m/g/java/basics/InheritanceThrowsClause.java
@@ -0,0 +1,32 @@
+package com.m.g.java.basics;
+
+import java.io.IOException;
+
+/**
+ * If a child class throws checked exception then parent should throws either same or its parent Exception.
+ *
+ */
+
+public class InheritanceThrowsClause {
+
+
+
+}
+class IParent
+{
+    public void m1() throws Exception {
+
+
+
+    }
+}
+
+class IChild extends IParent
+{
+   public void m1() throws IOException
+    {
+
+
+
+    }
+}
