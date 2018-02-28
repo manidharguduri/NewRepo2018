563c59f2ea9759189e39d41484284918141716ce
Manidhar.Guduri <Manidhar.Guduri@target.com>
2018-02-28 15:55:31 +0530

    Java : Basic : Comment Problem

---
+++ src/com/m/g/java/basics/CommentProblem.java
@@ -0,0 +1,26 @@
+package com.m.g.java.basics;
+
+public class CommentProblem {
+
+    public static void main(String[] args) {
+
+        // \u000d System.out.println("Comment Executed");
+
+        /**
+         * how the program is still executed when it is commented...
+         *
+         * because of the \u000d => this is a unique code character for new line,
+         * So the System.out.println is executed in the new line.
+         *
+         */
+
+    }
+}
+
+/**
+ *
+ * Output:
+ * =======
+ * Comment Executed
+ */
+
