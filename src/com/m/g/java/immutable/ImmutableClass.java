a06c9c34304f6eb527dc5b07532af3aec782912d
lbhallamudi <lbhallamudi@manh.com>
2018-03-03 01:12:04 +0530

    Java : Basic: immutability incomplete

--- src/com/m/g/java/immutable/ImmutableClass.java
+++ src/com/m/g/java/immutable/ImmutableClass.java
@@ -1,4 +1,32 @@
 package com.m.g.java.immutable;
 
-public class ImmutableClass {
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public final class ImmutableClass {
+   private final int id;
+   private final String name;
+   private List<String> alist;
+    ImmutableClass(int id,String name,String alist)
+    {
+        this.id=id;
+        this.name=name;
+        this.alist=new ArrayList<>();
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public int getId()
+    {
+        return id;
+    }
+
+    public List<String> getAlist() {
+        List<String> al=new ArrayList<>();
+        for(String n:al)
+        return alist;
+    }
 }
+
