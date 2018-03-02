ecb7ef9ad9e7661ff9cfc55b4c7378939d75f76f
lbhallamudi <lbhallamudi@manh.com>
2018-03-02 15:12:27 +0530

    Java : Operators : Complement operator Demo

---
+++ src/com/m/g/java/operator/ComplementOperator.java
@@ -0,0 +1,16 @@
+package com.m.g.java.operator;
+
+/**
+ * negation finds 2's complement -x-1;
+ *
+ */
+
+public class ComplementOperator {
+    public  static void main(String[] args) {
+        int i = 0;
+
+        int j = ~i;
+
+        System.out.println("Value of j :: " + j);
+    }
+}
