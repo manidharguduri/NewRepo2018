5e5cf0cfd25ad1ddffa5dc78b4e5deb7a21e0737
lbhallamudi <lbhallamudi@manh.com>
2018-03-06 12:48:42 +0530

    Java : Immutability : Implementation of Immutable class Using List

--- src/com/m/g/java/immutable/ImmutableClass.java
+++ src/com/m/g/java/immutable/ImmutableClass.java
@@ -1,32 +1,54 @@
 package com.m.g.java.immutable;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
+/**
+ * Whenever we have user/mutable objects in an immutable class.
+ * common sense points are:
+ *
+ * Two ways the immutability concept cna be violated.
+ *
+ * 1) if we change the list externally it may  violate this immutability nature if we directly
+ * assign the external object to our class object in the constructor.
+ *
+ * Solution : In our constructor we are creating a new object .Copy all the contents of external object to our class
+ * and intialize this new object to our class object.
+ *
+ * 2) another way to violate is get the list and can make changes on the list
+ *
+ * Solution: Create a new object in getter and copy all contents of the list to the new object and return the object.
+ */
+
 public final class ImmutableClass {
-   private final int id;
-   private final String name;
-   private List<String> alist;
-    ImmutableClass(int id,String name,String alist)
-    {
-        this.id=id;
-        this.name=name;
-        this.alist=new ArrayList<>();
+    private final int id;
+    private final String name;
+    private final List<String> alist;
+
+    ImmutableClass(int id, String name, List<String> extList) {
+        this.id = id;
+        this.name = name;
+        List<String> tempAlist = new ArrayList();
+        for (String s : extList) {
+            tempAlist.add(s);
+        }
+        this.alist = tempAlist;
     }
-    public String getName()
-    {
+
+    public String getName() {
         return name;
     }
-    public int getId()
-    {
+
+    public int getId() {
         return id;
     }
 
     public List<String> getAlist() {
-        List<String> al=new ArrayList<>();
-        for(String n:al)
-        return alist;
+        List<String> al = new ArrayList<>();
+        for (String s : this.alist) {
+            al.add(s);
+        }
+        return al;
     }
 }
 
